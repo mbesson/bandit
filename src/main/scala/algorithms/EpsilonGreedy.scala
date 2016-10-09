6712ab909ea07df1c81efeabcac33dd00ed609b6
mathieu <mathieu.besson@tuta.io>
2016-10-09 18:11:16 +0200

    Add a model for bandit problem

--- src/main/scala/algorithms/EpsilonGreedy.scala
+++ src/main/scala/algorithms/EpsilonGreedy.scala
@@ -1,8 +1,11 @@
 package algorithms
 
+import model.Bandit
+
 /**
   * Created by mathieu on 09/10/2016.
   */
-class EpsilonGreedy(epsilon: Float, counts: Int, values: Vector[Float]) {
+class EpsilonGreedy(bandit: Bandit, Float, counts: Vector[Int], values: Vector[Float]) {
+
 
 }
---
+++ src/main/scala/model/Bandit.scala
@@ -0,0 +1,10 @@
+package model
+
+/**
+  * Handles a MultiArmed Bandit problem.
+  *
+  * Created by mathieu on 09/10/2016.
+  */
+class Bandit(nbArm: Int) {
+
+}
